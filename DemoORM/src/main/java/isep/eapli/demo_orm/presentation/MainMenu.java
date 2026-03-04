0328c093c6db887f2dd14d6d834846b1190ec7dd
jrogalska19 <1252414@isep.ipp.pt>
2026-03-04 18:09:56 +0100

    english version of menu

--- DemoORM/src/main/java/isep/eapli/demo_orm/presentation/MainMenu.java
+++ DemoORM/src/main/java/isep/eapli/demo_orm/presentation/MainMenu.java
@@ -19,20 +19,17 @@ public class MainMenu {
 
 			switch (opcao) {
 				case 0:
-					System.out.println("fim ...");
+					System.out.println("Ending..."); [cite: 158]
 					break;
 				case 1:
-					System.out.println("Ainda não implementado ...");
+					System.out.println("Not yet implemented..."); [cite: 179]
 					break;
-
 				case 2:
-					System.out.println("Ainda não implementado ...");
+					System.out.println("Not yet implemented..."); [cite: 180]
 					break;
 
-				
-				
 				default:
-					System.out.println("opcao não reconhecida.");
+					System.out.println("Unrecognized option.");
 					break;
 			}
 		} while (opcao != 0);
@@ -43,14 +40,14 @@ public class MainMenu {
 		int option = -1;
 		System.out.println("");
 		System.out.println("=============================");
-		System.out.println(" Rent a Car ");
+		System.out.println(" Rent a Car "); [cite: 18]
 		System.out.println("=============================\n");
-		System.out.println("1.Registar Grupo Automóvel");
-		System.out.println("2.Listar todos os Grupos Automóveis");          
-		
-        System.out.println("=============================");
-        System.out.println("0. Sair\n\n");
-		option = Console.readInteger("Por favor escolha opção");
+		System.out.println("1. Register Car Group");
+		System.out.println("2. List All Car Groups");
+
+		System.out.println("=============================");
+		System.out.println("0. Exit\n\n");
+		option = Console.readInteger("Please choose an option");
 		return option;
 	}
 }
