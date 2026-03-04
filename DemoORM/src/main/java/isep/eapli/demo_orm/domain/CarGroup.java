a043d94ae4944acc615caf6b0bb4e65dd49aedc7
JStec03 <1252407@isep.ipp.pt>
2026-03-04 17:44:28 +0000

    persistence added

---
+++ DemoORM/src/main/java/isep/eapli/demo_orm/domain/CarGroup.java
@@ -0,0 +1,18 @@
+package isep.eapli.demo_orm.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+public class CarGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int doors;
+    private String carClass;
+}
--- DemoORM/src/main/java/isep/eapli/demo_orm/presentation/MainMenu.java
+++ DemoORM/src/main/java/isep/eapli/demo_orm/presentation/MainMenu.java
@@ -19,13 +19,13 @@ public class MainMenu {
 
 			switch (opcao) {
 				case 0:
-					System.out.println("Ending..."); [cite: 158]
+					System.out.println("Ending...");
 					break;
 				case 1:
-					System.out.println("Not yet implemented..."); [cite: 179]
+					System.out.println("Not yet implemented...");
 					break;
 				case 2:
-					System.out.println("Not yet implemented..."); [cite: 180]
+					System.out.println("Not yet implemented...");
 					break;
 
 				default:
@@ -38,9 +38,9 @@ public class MainMenu {
 
 	private static int menu() {
 		int option = -1;
-		System.out.println("");
+		System.out.println();
 		System.out.println("=============================");
-		System.out.println(" Rent a Car "); [cite: 18]
+		System.out.println(" Rent a Car ");
 		System.out.println("=============================\n");
 		System.out.println("1. Register Car Group");
 		System.out.println("2. List All Car Groups");
